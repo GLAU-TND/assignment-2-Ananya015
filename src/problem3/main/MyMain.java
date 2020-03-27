44c76760824cc685ddcb6c2ccb8a26022e8e48ba
Ananya015 <jainanaya78@gla.ac.in>
2020-03-28 02:28:55 +0530

    complete main class

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -6,11 +6,16 @@
  */
 package problem3.main;
 
+import problem3.myqueue.MyPriorityQueue;
 import problem3.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
+
 public class MyMain {
     public static void main(String[] args) {
+        MyPriorityQueue q = new MyPriorityQueue ();
+        q.enqueue ( new Student ( "Ananya", 07 ) );
 
     }
 }
