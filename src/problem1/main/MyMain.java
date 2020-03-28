d0aef77ca45cb8320439176a90a959e9612fcc3b
Ananya015 <jainanaya78@gla.ac.in>
2020-03-28 14:26:42 +0530

    call method in MyMain class

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,6 +7,19 @@
 package problem1.main;
 // executable class
 
-public class MyMain {
+import problem1.mybst.MyBinarySearchTree;
 
+public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree ();
+        tree.insert ( 30 );
+        tree.insert ( 24 );
+        tree.insert ( 7 );
+        tree.insert ( 89 );
+        tree.insert ( 55 );
+
+        tree.preOrder ( tree.getRoot () );
+        System.out.println ();
+        tree.inOrder ( tree.getRoot () );
+    }
 }
