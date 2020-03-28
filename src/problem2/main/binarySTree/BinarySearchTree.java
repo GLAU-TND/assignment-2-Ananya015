213a2c924583a237f0b7eef71852e71332ddb069
Ananya015 <jainanaya78@gla.ac.in>
2020-03-28 16:17:26 +0530

    initialise variable

--- src/problem2/main/binarySTree/BinarySearchTree.java
+++ src/problem2/main/binarySTree/BinarySearchTree.java
@@ -1,6 +1,13 @@
 package problem2.main.binarySTree;
 
 
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+
 public class BinarySearchTree {
+    private TreeNode root;
+    private int countRight;
+    private MyQueue preQueue;
+    private MyQueue postQueue;
 
 }
